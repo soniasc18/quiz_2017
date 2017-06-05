6cb9d31db307e851480e0804aa04b48f4b1ca1f9
sonia <soniasc18@hotmail.com>
2017-06-05 19:09:10 +0200

    dfasfasdf

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -196,69 +196,76 @@ exports.check = function (req, res, next) {
 
 
 
+
+
+
 // GET /quizzes/randomplay
-exports.random_play = function (req, res, next){
+exports.random_play = function (req, res, next) {
 
-	//Si no existe el array de preguntas contestadas lo creo inicializado a -1
-	if(!req.session.aciertos)
-		req.session.aciertos=0;
-	if(!req.session.hechas)
-		req.session.hechas=[-1];
+    if(!req.session.randomPlay){
+        req.session.randomPlay= {
+            resueltos: []};
+    }
+    // array con las preguntas ya resultas
+    var used = req.session.randomPlay.resueltos.length ? req.session.randomPlay.resueltos : [-1];
 
-	//Contamos las preguntas que NO estan hechas aun
-	models.Quiz.count({where:{id:{$notIn:req.session.hechas}}})
-		.then(function(c){ //c es un entero 
-			//var a=Math.floor(Math.random()*c});
-		        var r=models.Quiz.findAll({ //r es un array con todas las preguntas NO hechas
-			where:{id:{$notIn:req.session.hechas}}
-			});
-       		 return r;
-		}).then(function(noHechas){ //noHechas es el array r que retornaba justo antes
-			if(noHechas.length===0){
-        		        //req.session.aciertos=0;
-	               		req.session.hechas=[-1];
-				res.render('quizzes/random_nomore', {
-				score:req.session.aciertos
-			});
-			}else{
-				var a=Math.floor(Math.random()*noHechas.length);
-				var q=noHechas[a];
-		                req.session.hechas.push(q.id);
-                		//if(req.session.aciertos!==0) ++req.session.aciertos;
-				res.render('quizzes/random_play', {
-				quiz:q,
-		                score:req.session.aciertos++
+    var whereOpt = {'id':{$notIn:used}};
+
+
+    models.Quiz.count(whereOpt)
+        .then(function (nQuestions) {
+
+            var idQuestion = Math.floor(Math.random() * nQuestions);
+
+          var findOptions = {
+                limit:1,
+                'id': {$gt: idQuestion},
+                where: whereOpt
+            };
+            return models.Quiz.findAll(findOptions);
+        })
+        .then(function (question) {
+
+            var numAciertos = req.session.randomPlay.resueltos.length;
+            var quiz = question[0];
+            if(!quiz) {
+                req.session.randomPlay.resueltos = [];
+                res.render('quizzes/random_nomore', {
+                    score: numAciertos
+                });
+            } else {
+                //var quiz =question[0];
+                //ar numAciertos = req.session.numAciertos ? req.session.numAciertos : 0;
+
+                req.session.randomPlay.resueltos.push(quiz.id);
+
+                res.render('quizzes/random_play', {
+                    score: numAciertos,
+                    quiz: quiz
                 });
             }
-        }).catch(function(error) {
-        req.flash('error', 'Error al cargar el Quiz: ' + error.message);
+
+
+    }).catch(function (error) {
         next(error);
     });
 };
-
-
-//GET /quizzes/randomcheck/:quizId?answer=respuesta
-
-//vamos a tener que recuperar lo introducido en el formulario
-//	req.quiz.question = req.body.question;
-//	req.quiz.answer = req.body.answer;
-exports.random_check = function (req, res, next){
-
-    //Si no existe el array de preguntas contestadas lo creo inicializado a -1
-    if(!req.session.practica52) 
-        req.session.practica52=0;
-    if(!req.session.hechas)
-        req.session.hechas=[-1];
-
+// GET /quizzes/randomcheck
+exports.random_check = function (req, res, next) {
     var answer = req.query.answer || "";
 
     var result = answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim();
+    var numAciertos = req.session.randomPlay.resueltos.length;
+    if(!result){
+        numAciertos=numAciertos -1;
+        req.session.randomPlay.resueltos = [];
+    }
 
     res.render('quizzes/random_result', {
         quiz: req.quiz,
         result: result,
-        answer: answer,
-        score:req.session.aciertos
+        score: numAciertos,//req.session.randomPlay.resueltos.length,
+        answer: answer
     });
 
-};
+}
